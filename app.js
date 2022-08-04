d2b94c4d038e099b41249b6bc0dfba99c0536432
Maksimgnam <107640396+Maksimgnam@users.noreply.github.com>
2022-08-04 16:23:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$("[data-scroll]").on('click', function(event){
+    event.preventDefault();
+    
+    let blockId = $(this).data('scroll')
+        blockOffset = $(blockId).offset().top;
+        $('html', 'body').animate({
+            scrollTop: blockOffset
+        }, 500);   
+
+})
